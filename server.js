b9289a237b412d9ca0550153df771d8ae69e907e
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-22 00:21:56 +0700

    Membuat basic server express.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "public/index.html");
+});
+
+const port = 4000;
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
