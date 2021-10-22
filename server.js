1abc83ba3c13c8391b2df991fd023e8c82a6acc8
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-22 09:57:39 +0700

    Finishing Project

--- server.js
+++ server.js
@@ -21,6 +21,31 @@ app.get("/video-info", async (req, res) => {
   }
 });
 
+app.get("/video-download", async (req, res) => {
+  try {
+    const videoUrl = req.query.videoUrl;
+    const itag = req.query.itag;
+
+    const info = await ytdl.getInfo(videoUrl);
+    const formats = info.formats.filter((format) => {
+      return format.itag == itag;
+    });
+
+    res.header(
+      "Content-Disposition",
+      `attachment; filename="${info.videoDetails.title}.mp4"`
+    );
+    res.setHeader("Content-type", "video/mp4");
+    res.setHeader("Content-Length", formats[0].contentLength);
+
+    ytdl(videoUrl, {
+      filter: (format) => format.itag == itag,
+    }).pipe(res);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
 const port = 4000;
 app.listen(port, () => {
   console.log("Server started on port " + port);
