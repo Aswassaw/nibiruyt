92b61996b84daa4407529b3f8e7126caa325784c
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-22 10:04:42 +0700

    Install dotenv, helmet, dan compression

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const ytdl = require("ytdl-core");
+const compression = require("compression");
+const helmet = require("helmet");
+const chalk = require("chalk");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(compression());
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "public/index.html");
@@ -46,7 +52,10 @@ app.get("/video-download", async (req, res) => {
   }
 });
 
-const port = 4000;
-app.listen(port, () => {
-  console.log("Server started on port " + port);
+// Running Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+  console.log(chalk`Visit {rgb(128, 237, 153) http://localhost:${PORT}}`);
+  console.log(chalk`Developed by {rgb(255, 92, 88) Andry Pebrianto}`);
 });
